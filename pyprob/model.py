fix address transition loss to be negative logprob

pyprob/model.py
@@ -11,7 +11,6 @@ from . import util, state, TraceMode, PriorInflation, InferenceEngine, Inference
 from .nn import InferenceNetwork as InferenceNetworkBase
 from .nn import OnlineDataset, OfflineDataset, InferenceNetworkFeedForward, InferenceNetworkLSTM, SurrogateNetworkLSTM
 from .remote import ModelServer
-from .trace import Variable, Trace
 
 
 class Model():
@@ -230,44 +229,8 @@ class Model():
             self._inference_network = None
         else:
             self._inference_network = self._inference_surrogate_network
-        self.forward = self._surrogate_forward
+        self.forward = self._surrogate_network.get_surrogate_forward(self._original_forward)
         print('Finished training surrogate model. Any further analysis is made using this surrogate model!')
-
-    def _surrogate_forward(self, *args, **kwargs):
-        """
-       Rewrite the forward function otherwise specified by the user.
-
-        This forward function uses the surrogate model as joint distribution
-
-        """
-        # sample initial address
-        address = self._surrogate_network._layers_address_transitions["init"](None).sample()
-        prev_variable = None
-        while address != "end":
-            surrogate_dist = self._surrogate_network._layers_surrogate_distributions[address]
-            current_variable = Variable(distribution=surrogate_dist.dist_type,
-                                        address=address, value=None)
-            _, lstm_output = self._surrogate_network.run_lstm_step(current_variable, prev_variable)
-            address_dist = self._surrogate_network._layers_address_transitions[address]
-            lstm_output = lstm_output.squeeze(0) # squeeze the sequence dimension
-            dist = surrogate_dist(lstm_output)
-            value = state.sample(distribution=dist)
-            prev_variable = Variable(distribution=surrogate_dist.dist_type, address=address, value=value)
-
-            smp = util.to_tensor(value)
-            sample_embedding = self._surrogate_network._layers_sample_embedding[address](smp)
-            address_transition_input = torch.cat([lstm_output, sample_embedding], dim=1)
-            a_dist = address_dist(address_transition_input)
-            address = a_dist.sample()
-
-            if address == "unknown":
-                # if an address is unknown default to the simulator
-                # by resetting the _current_trace
-                state._current_trace = Trace()
-                self._original_forward(*args, **kwargs)
-                break
-
-        return None
 
     def save_inference_network(self, file_name):
         if self._inference_network is None:

pyprob/nn/surrogate_address_transition.py
@@ -8,15 +8,15 @@ from ..distributions import Categorical
 
 class SurrogateAddressTransition(nn.Module):
 
-    def __init__(self, input_shape, address=None, num_layers=2, first_address=False, last_address=False):
+    def __init__(self, input_shape, next_address=None, num_layers=2, first_address=False, last_address=False):
         super().__init__()
         input_shape = util.to_size(input_shape)
         self._output_shape = torch.Size([2])
         self._first_address = first_address
         self._last_address = last_address
-        if address:
-            self._addresses = [address]
-            self._address_to_class = {address: torch.Tensor([0])}
+        if next_address:
+            self._addresses = [next_address]
+            self._address_to_class = {next_address: torch.Tensor([0])}
         else:
             self._addresses = ["end"]
             self._address_to_class = {"end": torch.Tensor([0])}
@@ -26,7 +26,7 @@ class SurrogateAddressTransition(nn.Module):
                                         num_layers=num_layers,
                                         activation=torch.relu,
                                         activation_last=None)
-        self._softmax = nn.Softmax(dim=0)
+        self._softmax = nn.Softmax(dim=1)
 
         self._total_train_iterations = 0
         self._n_classes = 1
@@ -52,8 +52,8 @@ class SurrogateAddressTransition(nn.Module):
         pass
 
     def loss(self, next_addresses):
-        classes = self._transform_to_class(next_addresses).unsqueeze(1)
-        loss = self._categorical.log_prob(classes)
+        classes = self._transform_to_class(next_addresses).to(device=util._device)
+        loss = -self._categorical.log_prob(classes)
         return loss
 
     def _update_transforms(self):

pyprob/nn/surrogate_network_lstm.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 from termcolor import colored
 
 from . import EmbeddingFeedForward, InferenceNetwork, SurrogateAddressTransition, SurrogateNormal
-from .. import util
+from .. import util, state
 from ..distributions import Normal, Uniform, Categorical, Poisson
+from ..trace import Variable, Trace
 
 
 class SurrogateNetworkLSTM(InferenceNetwork):
@@ -232,3 +233,44 @@ class SurrogateNetworkLSTM(InferenceNetwork):
 
             batch_loss += sub_batch_loss + surrogate_loss
         return True, batch_loss / batch.size
+
+    def get_surrogate_forward(self, original_forward=lambda x: x):
+        self._original_forward = original_forward
+        return self._surrogate_forward
+
+    def _surrogate_forward(self, *args, **kwargs):
+        """
+        Rewrite the forward function otherwise specified by the user.
+
+        This forward function uses the surrogate model as joint distribution
+
+        """
+        # sample initial address
+        address = self._layers_address_transitions["init"](None).sample()
+        prev_variable = None
+        while address != "end":
+            surrogate_dist = self._layers_surrogate_distributions[address]
+            current_variable = Variable(distribution=surrogate_dist.dist_type,
+                                        address=address, value=None)
+            _, lstm_output = self.run_lstm_step(current_variable, prev_variable)
+            address_dist = self._layers_address_transitions[address]
+            lstm_output = lstm_output.squeeze(0) # squeeze the sequence dimension
+            dist = surrogate_dist(lstm_output)
+            value = state.sample(distribution=dist)
+            prev_variable = Variable(distribution=surrogate_dist.dist_type, address=address, value=value)
+
+            smp = util.to_tensor(value)
+            sample_embedding = self._layers_sample_embedding[address](smp)
+            address_transition_input = torch.cat([lstm_output, sample_embedding], dim=1)
+            a_dist = address_dist(address_transition_input)
+            address = a_dist.sample()
+
+            if address == "unknown":
+                # if an address is unknown default to the simulator
+                # by resetting the _current_trace
+                state._current_trace = Trace()
+                self._original_forward(*args, **kwargs)
+                break
+
+        return None
+