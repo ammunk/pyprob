f3ea7016b5c3a14a894eae42eb8751128a196043
Andreas M. Munk <andreas.m.m64@gmail.com>
2019-05-22 15:19:07 -0700

    update surrogate_normal_w_deconv

--- pyprob/nn/surrogate_normal_w_deconv.py
+++ pyprob/nn/surrogate_normal_w_deconv.py
@@ -22,40 +22,50 @@ class SurrogateNormalConvTranspose2d(nn.Module):
                                         activation=torch.relu, activation_last=None)
         H = mean_shape[0]
         W = mean_shape[1]
-        # self.deconv = nn.Sequential(
-        #     nn.ConvTranspose2d(1,1, kernel_size=(50, 8)),
-        #     nn.ReLU(),
-        #     nn.ConvTranspose2d(1,1, kernel_size=(100, 10)),
-        #     nn.ReLU(),
-        #     nn.ConvTranspose2d(1,1, kernel_size=(H-(self._output_dim_mean+150 - 2)+1, W-(W_input+10+8-2)+1))
-        # )
-
-        output_channel1 = max(int(self._output_dim_mean*0.3),1)
-        output_channel2 = max(int(output_channel1*0.3),1)
-        output_channel3 = max(int(output_channel2*0.3),1)
-        output_channel4 = max(int(output_channel3*0.3),1)
-        output_channel5 = max(int(output_channel4*0.3),1)
-        output_channel6 = max(int(output_channel5*0.3),1)
-        output_channel7 = max(int(output_channel6*0.3),1)
-        self._ff_means = nn.Linear(self._output_dim_mean, output_channel1*16) # make a output_channel1 x 4 x 4 (by reshaping)
+        output_channel1 = max(int(self._output_dim_mean*0.5),1)
+        output_channel2 = max(int(output_channel1*0.5),1)
+        output_channel3 = max(int(output_channel2*0.5),1)
+        output_channel4 = max(int(output_channel3*0.5),1)
+        output_channel5 = max(int(output_channel4*0.5),1)
+        output_channel6 = max(int(output_channel5*0.5),1)
+        output_channel7 = max(int(output_channel6*0.5),1)
+        self._ff_means = nn.Linear(self._output_dim_mean, output_channel1*25) # make a output_channel1 x 4 x 4 (by reshaping)
         self._deconv = nn.Sequential(
-            nn.Upsample(size=(10,4),mode="nearest"),
-            nn.Conv2d(output_channel1, output_channel2, kernel_size=(3,3), padding=(1,1)), # output is output_channel2 x 10 x 4
-            nn.ReLU(),
-            nn.Upsample(size=(20,6),mode="nearest"),
-            nn.Conv2d(output_channel2, output_channel3, kernel_size=(5,5), padding=(2,2)), # output is output_channel3 x 20 x 6
-            nn.ReLU(),
-            nn.Upsample(size=(40, 15),mode="nearest"),
-            nn.Conv2d(output_channel3, output_channel4, kernel_size=(2,2), padding=(1,1)), # output is output_channel4 x 40 x 15
-            nn.ReLU(),
-            nn.Upsample(size=(80, 20),mode="nearest"),
-            nn.Conv2d(output_channel4, output_channel5, kernel_size=(3,2), padding=(11,3)), # output is output_channel5 x 100 x 25
-            nn.Upsample(size=(250, 28),mode="nearest"),
-            nn.Conv2d(output_channel5, output_channel6, kernel_size=(5,5), padding=(27,2)), # output is output_channel6 x 300 x 28
-            nn.Upsample(size=(500, 30),mode="nearest"),
-            nn.Conv2d(output_channel6, output_channel7, kernel_size=(3,1), padding=(1,0)), # output is output_channel7 x 300 x 28
-            nn.Upsample(size=(H, W), mode="nearest"),
-            nn.Conv2d(output_channel7, 1, kernel_size=(3,3), padding=(1,1)) # output is 1 x H x W
+
+            nn.Upsample(size=(10+4,4+4), mode="nearest"),
+            nn.Conv2d(output_channel1, output_channel2, kernel_size=(3,3), padding=0),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(output_channel2, output_channel2, kernel_size=(3,3), padding=0),
+            nn.LeakyReLU(inplace=True),
+
+            nn.Upsample(size=(20+4,6+4), mode="nearest"),
+            nn.Conv2d(output_channel2, output_channel3, kernel_size=(3,3), padding=0),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(output_channel3, output_channel3, kernel_size=(3,3), padding=0),
+            nn.LeakyReLU(inplace=True),
+
+            nn.Upsample(size=(40+4, 12+4), mode="nearest"),
+            nn.Conv2d(output_channel3, output_channel4, kernel_size=(3,3), padding=0),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(output_channel4, output_channel4, kernel_size=(3,3), padding=0),
+            nn.LeakyReLU(inplace=True),
+
+            nn.Upsample(size=(80+4, 20+4),mode="nearest"),
+            nn.Conv2d(output_channel4, output_channel5, kernel_size=(3,3), padding=0),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(output_channel5, output_channel5, kernel_size=(3,3), padding=0),
+            nn.LeakyReLU(inplace=True),
+
+            nn.Upsample(size=(160+4, 25+4),mode="nearest"),
+            nn.Conv2d(output_channel5, output_channel6, kernel_size=(3,3), padding=0),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(output_channel6, output_channel6, kernel_size=(3,3), padding=0),
+            nn.LeakyReLU(inplace=True),
+
+            nn.Upsample(size=(H+4, W+4), mode="nearest"),
+            nn.Conv2d(output_channel6, output_channel7, kernel_size=(3,3), padding=0),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(output_channel7, 1, kernel_size=(3,3), padding=0), # output is 1 x H x W
         )
         self._total_train_iterations = 0
 
@@ -71,7 +81,7 @@ class SurrogateNormalConvTranspose2d(nn.Module):
         batch_size = x.size(0)
         x = self._ff(x)
         means = x[:, :self._output_dim_mean].view(batch_size, -1)
-        means = self._ff_means(means).view(batch_size, -1, 4, 4)
+        means = self._ff_means(means).view(batch_size, -1, 5, 5)
         self.means = self._deconv(means).squeeze(1) # squeeze the channel
 
         self.stddevs = torch.exp(x[:, self._output_dim_mean:]).view(self._var_output_shape)
@@ -84,9 +94,10 @@ class SurrogateNormalConvTranspose2d(nn.Module):
         return Normal(self.means, self.stddevs)
 
     def loss(self, distributions):
+        bs = len(distributions)
         simulator_means = self._transform_mean(distributions)
         simulator_stddevs = self._transform_stddev(distributions)
-        p_normal = Normal(simulator_means, simulator_stddevs)
-        q_normal = Normal(self.means, self.stddevs)
+        p_normal = Normal(simulator_means.view(bs,-1), simulator_stddevs.view(bs,-1))
+        q_normal = Normal(self.means.view(bs,-1), self.stddevs.view(bs,-1))
 
         return Distribution.kl_divergence(p_normal, q_normal)
